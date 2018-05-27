3f6c8a30d8423806892446c1b2dc78c564244115
Trinh Duc Thien <td.thien@shopmacher.de>
2018-05-27 13:01:05 +0700

    fosckeditor

--- src/Form/Util/FormTypeHelper.php
+++ src/Form/Util/FormTypeHelper.php
@@ -100,7 +100,7 @@ final class FormTypeHelper
         'easyadmin_group' => EasyAdminGroupType::class,
         'easyadmin_section' => EasyAdminSectionType::class,
         // Popular third-party bundles types
-        'ckeditor' => 'Hillrange\\CKEditor\\Form\\CKEditorType',
+        'ckeditor' => 'FOS\\CKEditorBundle\\Form\\Type\\CKEditor',
         'vich_file' => 'Vich\\UploaderBundle\\Form\\Type\\VichFileType',
         'vich_image' => 'Vich\\UploaderBundle\\Form\\Type\\VichImageType',
     ];
